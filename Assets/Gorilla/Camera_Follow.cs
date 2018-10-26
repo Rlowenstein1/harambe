d46e069bf08458d871c17377082e3a209862c036
dtrementozzi3 <dtremnz@gatech.edu>
2018-10-26 15:24:58 -0400

    Gorilla moves without animation and camera follows

---
+++ Assets/Gorilla/Camera_Follow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Camera_Follow : MonoBehaviour
+{
+    public Rigidbody gorilla;
+    public float smoothTime = 1f;		// a public variable to adjust smoothing of camera motion
+    public float maxSpeed = 50f;        //max speed camera can move
+    public Transform desiredPose;           // the desired pose for the camera, specified by a transform in the game
+
+    protected Vector3 currentPositionCorrectionVelocity;
+    protected Vector3 currentFacingCorrectionVelocity;
+
+
+    void LateUpdate()
+    {
+        desiredPose = gorilla.transform.Find("CamPos");
+        if (desiredPose != null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, desiredPose.position, ref currentPositionCorrectionVelocity, smoothTime, maxSpeed, Time.deltaTime);
+            transform.forward = Vector3.SmoothDamp(transform.forward, desiredPose.forward, ref currentFacingCorrectionVelocity, smoothTime, maxSpeed, Time.deltaTime);
+        }
+    }
+}
+
---
+++ Assets/Gorilla/Gorilla_Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gorilla_Move : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, -1 * speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+	}
+}
