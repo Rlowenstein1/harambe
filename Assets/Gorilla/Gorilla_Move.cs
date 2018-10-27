38374d8c364a1c205039f49ad02b4b7c77da01f0
dtrementozzi3 <dtremnz@gatech.edu>
2018-10-27 14:26:06 -0400

    Gorilla turns and moves faster

--- Assets/Gorilla/Gorilla_Move.cs
+++ Assets/Gorilla/Gorilla_Move.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Gorilla_Move : MonoBehaviour {
 
-    public float speed;
+    Quaternion targetRotation;
+    public float speed, forward, turn;
+    Rigidbody gorilla;
+
+    public Quaternion Rotation
+    {
+        get { return targetRotation; }
+    }
 
 	// Use this for initialization
 	void Start () {
-        speed = 10f;
+        speed = 20f;
+        targetRotation = transform.rotation;
+        gorilla = GetComponent<Rigidbody>();
+        forward = 0;
+        turn = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, -1 * speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+        forward = Input.GetAxis("Vertical");
+        turn = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(turn) > 0.1f)
+        {
+            targetRotation *= Quaternion.AngleAxis(100 * turn * Time.deltaTime, Vector3.forward);
+        }
+        transform.rotation = targetRotation;
 	}
+
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(forward) > 0.1f)
+        {
+            gorilla.velocity = -1 * transform.up * forward * speed;
+        } else
+        {
+            gorilla.velocity = Vector3.zero;
+        }
+    }
 }
