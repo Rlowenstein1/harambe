ef44e4945bb987101144708b4fd74ee3b6426a21
dtrementozzi3 <dtremnz@gatech.edu>
2018-10-28 18:25:00 -0400

    Working gorilla animations

--- Assets/Gorilla/Gorilla_Move.cs
+++ Assets/Gorilla/Gorilla_Move.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Gorilla_Move : MonoBehaviour {
 
     Quaternion targetRotation;
     public float speed, forward, turn;
     Rigidbody gorilla;
+    public Animator anim;
 
     public Quaternion Rotation
     {
@@ -18,14 +20,18 @@ public class Gorilla_Move : MonoBehaviour {
         speed = 20f;
         targetRotation = transform.rotation;
         gorilla = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         forward = 0;
         turn = 0;
-	}
+        anim.applyRootMotion = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         forward = Input.GetAxis("Vertical");
         turn = Input.GetAxis("Horizontal");
+       
+        //anim.SetFloat("velx", turn);
         if (Mathf.Abs(turn) > 0.1f)
         {
             targetRotation *= Quaternion.AngleAxis(100 * turn * Time.deltaTime, Vector3.forward);
@@ -42,5 +48,7 @@ public class Gorilla_Move : MonoBehaviour {
         {
             gorilla.velocity = Vector3.zero;
         }
+        anim.SetFloat("velx", gorilla.velocity.magnitude/20);
+        Debug.Log(gorilla.velocity.magnitude/20);
     }
 }
