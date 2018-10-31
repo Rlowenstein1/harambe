6bef3b9728ecc7f8ddc59c88f20ccee83933eac0
dtrementozzi3 <dtremnz@gatech.edu>
2018-10-31 12:32:01 -0400

    Attack animation added and functional

--- Assets/Gorilla/Gorilla_Move.cs
+++ Assets/Gorilla/Gorilla_Move.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Animator))]
 public class Gorilla_Move : MonoBehaviour {
 
     Quaternion targetRotation;
-    public float speed, forward, turn, run;
-    Rigidbody gorilla;
+    public float speed, forward, turn, run, attack;
+    public Rigidbody gorilla;
     public Animator anim;
 
     public Quaternion Rotation
@@ -16,37 +17,48 @@ public class Gorilla_Move : MonoBehaviour {
     }
 
 	// Use this for initialization
-	void Start () {
-        speed = 10f;
+	void Awake () {
+        speed = 5f;
         targetRotation = transform.rotation;
         gorilla = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         forward = 0;
         turn = 0;
         run = 0;
-	}
+        attack = 0;
+        anim.applyRootMotion = true;
+     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         forward = Input.GetAxis("Vertical");
         turn = Input.GetAxis("Horizontal");
         run = Input.GetAxis("Run");
-        if (run != 0 && speed <=20)
+        attack = Input.GetAxis("Fire1");
+        if (attack != 0)
+        {
+            anim.SetTrigger("Attack");
+        } 
+        if (run != 0 && speed <=8)
         {
             speed += 1;
         } else if (run == 0)
         {
-            if (speed >= 10)
-            {
-                speed = speed - 1;
-            }
+        if (speed >= 5)
+        {
+           speed = speed - 1;
         }
+        }
+    
+        
         if (Mathf.Abs(turn) > 0.1f)
         {
             targetRotation *= Quaternion.AngleAxis(100 * turn * Time.deltaTime, Vector3.forward);
         }
         transform.rotation = targetRotation;
 
+        print(forward * Time.deltaTime * speed);
+        anim.SetFloat("velx", forward * Time.deltaTime * speed);
 
         if (Input.anyKeyDown)
         {
@@ -56,15 +68,9 @@ public class Gorilla_Move : MonoBehaviour {
         }
     }
 
-    void FixedUpdate()
-    {
-        if (Mathf.Abs(forward) > 0.1f)
-        {
-            gorilla.velocity = -1 * transform.up * forward * speed;
-        } else
-        {
-            gorilla.velocity = Vector3.zero;
-        }
-        anim.SetFloat("velx", gorilla.velocity.magnitude / 20);
+    void OnAnimatorMove()
+    {      
+        this.transform.position = anim.rootPosition;
+        print(anim.rootPosition);
     }
 }
