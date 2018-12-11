a0519ea8ec9d87a9d5ca49d47afb66d31d991b6b
taylorhavrilak <thavrilak3@gatech.edu>
2018-12-11 14:07:29 -0500

    More buildings

--- Assets/Scripts/Building/BuildingHealth.cs
+++ Assets/Scripts/Building/BuildingHealth.cs
@@ -9,6 +9,9 @@ public class BuildingHealth : MonoBehaviour {
 
     public GameObject prefabBuilding = null;
 
+    public GameObject dustParticles = null;
+    public GameObject dust = null;
+
     void Awake()
     {
         currentHealth = startingHealth;
@@ -17,7 +20,8 @@ public class BuildingHealth : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-
+        dustParticles = GameObject.FindGameObjectWithTag("Dust");
+        dust = Instantiate(dustParticles, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -30,13 +34,18 @@ public class BuildingHealth : MonoBehaviour {
     {
         currentHealth -= amount;
 
+        // When attacked, emit particles from the prefab for a duration
+        ParticleSystem ps = dust.GetComponent<ParticleSystem>();
+        ps.Emit(2000);
+
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
+
+            // Instantiate new damaged building model in place of the current
             var damagedBuilding = Instantiate(prefabBuilding, transform.position, Quaternion.identity);
             damagedBuilding.gameObject.tag = "Building";
             damagedBuilding.AddComponent<BuildingHealth>();
         }
     }
-
 }
